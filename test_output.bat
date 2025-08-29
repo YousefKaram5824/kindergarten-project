0f1295ec32bd8cd7c0d0223d3adceb6fcbba21f7
Yousef Karam <1020118@stemegypt.edu.eg>
2025-08-29 12:22:33 +0300

    .

--- test_output.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Testing Python output...
-python -c "print('Hello World from Python')"
-python -c "import os; print('Current directory:', os.getcwd())"
-python -c "import os; print('Files in directory:'); [print(f) for f in os.listdir('.')]"
-pause
